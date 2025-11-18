396584c8afc2aa9d9cb47da2185df912ac17e7f0
riAnriAn0 <ml2463265@gmail.com>
2025-11-18 12:51:48 -0300

    organização do código principal

--- detect.py
+++ detect.py
@@ -1,4 +1,3 @@
-"""Main script to run the object detection routine."""
 import argparse
 import sys
 import time
@@ -8,6 +7,14 @@ import numpy as np
 from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
+
+
+MODEL_PATH = 'modelos/efficientdet_lite0.tflite'
+cAM_ID = 0
+FRAME_WIDTH = 160
+FRAME_HEIGHT = 120
+NUM_THREADS = 4
+ENABLE_EDGETPU = False
 
 _MARGIN = 10  # pixels
 _ROW_SIZE = 10  # pixels
@@ -19,15 +26,7 @@ def visualize(
     image: np.ndarray,
     detection_result: processor.DetectionResult,
 ) -> np.ndarray:
-  """Draws bounding boxes on the input image and return it.
 
-  Args:
-    image: The input RGB image.
-    detection_result: The list of all "Detection" entities to be visualize.
-
-  Returns:
-    Image with bounding boxes.
-  """
   for detection in detection_result.detections:
     # Draw bounding_box
     bbox = detection.bounding_box
@@ -49,17 +48,7 @@ def visualize(
 
 def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
         enable_edgetpu: bool) -> None:
-  """Continuously run inference on images acquired from the camera.
-
-  Args:
-    model: Name of the TFLite object detection model.
-    camera_id: The camera id to be passed to OpenCV.
-    width: The width of the frame captured from the camera.
-    height: The height of the frame captured from the camera.
-    num_threads: The number of CPU threads to run the model.
-    enable_edgetpu: True/False whether the model is a EdgeTPU model.
-  """
-
+  
   # Variables to calculate FPS
   counter, fps = 0, 0
   start_time = time.time()
@@ -137,42 +126,13 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
   cv2.destroyAllWindows()
 
 def main():
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument(
-      '--model',
-      help='Path of the object detection model.',
-      required=False,
-      default='ssd_mobilenetv1/2.tflite')
-  parser.add_argument(
-      '--cameraId', help='Id of camera.', required=False, type=int, default=0   )
-  parser.add_argument(
-      '--frameWidth',
-      help='Width of frame to capture from camera.',
-      required=False,
-      type=int,
-      default=640)
-  parser.add_argument(
-      '--frameHeight',
-      help='Height of frame to capture from camera.',
-      required=False,
-      type=int,
-      default=640)
-  parser.add_argument(
-      '--numThreads',
-      help='Number of CPU threads to run the model.',
-      required=False,
-      type=int,
-      default=4)
-  parser.add_argument(
-      '--enableEdgeTPU',
-      help='Whether to run the model on EdgeTPU.',
-      action='store_true',
-      required=False,
-      default=False)
-  args = parser.parse_args()
-
-  run(args.model, int(args.cameraId), args.frameWidth, args.frameHeight,int(args.numThreads), bool(args.enableEdgeTPU))
+  run(
+      MODEL_PATH,
+      cAM_ID,
+      FRAME_WIDTH,
+      FRAME_HEIGHT,
+      NUM_THREADS,
+      ENABLE_EDGETPU)
 
 if __name__ == '__main__':
   main()
